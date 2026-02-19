5bd0d3dcb56c291ad1ca9e3bfc05a9e71895a5cc
Juan Hinojosa <juanma.hinojosa97@gmail.com>
2026-02-19 13:08:58 -0300

    cors import

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors')
 const connectDB = require('./config/db');
 const entradaRoutes = require('./routes/entradaRoutes')
 const corsOptions = require('./config/cors');
