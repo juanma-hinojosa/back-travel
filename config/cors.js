d464e3503db91f6d28cd666151c71a7e6b971e13
Juan Hinojosa <juanma.hinojosa97@gmail.com>
2026-02-19 22:53:03 -0300

    url cors

--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   // "http://localhost:5173",          // Vite local
-  "https://nosso-date-rafa-juan.netlify.app/",         // dominio de producción
+  "https://nosso-date-rafa-juan.netlify.app",         // dominio de producción
 ];
 
 const corsOptions = {
